8a739989ad622c05bacbac105d66f427027dfc87
Rudy Lee <rudylee1988@gmail.com>
2013-08-16 16:43:35 +1000

    adding bunch of cookbooks for rails role

--- roles/php5.rb
+++ roles/php5.rb
@@ -3,22 +3,22 @@ name "php5"
 
 # Run list function we mentioned earlier
 run_list(
-    "recipe[apt]",
-    "recipe[openssl]",
-    "recipe[apache2]",
-    "recipe[apache2::mod_php5]",
-    "recipe[mysql]",
-    "recipe[mysql::server]",
-    "recipe[php]",
-    "recipe[php::module_mysql]",
-    "recipe[apache2::vhosts]",
-    "recipe[database::mysql]"
+  "recipe[apt]",
+  "recipe[openssl]",
+  "recipe[apache2]",
+  "recipe[apache2::mod_php5]",
+  "recipe[mysql]",
+  "recipe[mysql::server]",
+  "recipe[php]",
+  "recipe[php::module_mysql]",
+  "recipe[apache2::vhosts]",
+  "recipe[database::mysql]"
 )
 
 override_attributes(
-    "mysql" => {
-        "server_root_password" => '',
-        "server_repl_password" => '',
-        "server_debian_password" => ''
-    }
+  "mysql" => {
+    "server_root_password" => '',
+    "server_repl_password" => '',
+    "server_debian_password" => ''
+  }
 )
---
+++ roles/rails.rb
@@ -0,0 +1,32 @@
+# Name of the role should match the name of the file
+name "rails"
+
+# Run list function we mentioned earlier
+run_list(
+  "recipe[apt]",
+  "recipe[git]",
+  "recipe[sqlite]",
+  "recipe[openssl]",
+  "recipe[mysql::client]",
+  "recipe[mysql::ruby]",
+  "recipe[mysql::server]",
+  "recipe[postgresql::ruby]",
+  "recipe[postgresql::server]",
+  "recipe[ruby_build]",
+  "recipe[rbenv::system]",
+  "recipe[rbenv::vagrant]",
+  "recipe[database]"
+)
+
+override_attributes(
+  "mysql" => {
+    "server_root_password" => '',
+    "server_repl_password" => '',
+    "server_debian_password" => ''
+  },
+  "postgresql" => {
+    "password" => {
+      'postgres' => ''
+    }
+  }
+)
